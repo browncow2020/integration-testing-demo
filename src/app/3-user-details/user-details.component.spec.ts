1519f4ef62a324be3fa04bfe65479a0ac131ef09
Browncow2020 <connectwithbob@icloud.com>
2018-09-23 11:14:01 -0500

    First commit

---
+++ src/app/3-user-details/user-details.component.spec.ts
@@ -0,0 +1,48 @@
+/* tslint:disable:no-unused-variable */
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { DebugElement } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import { Observable } from 'rxjs';
+
+import { UserDetailsComponent } from './user-details.component';
+
+class RouterStub {
+  navigate(params) {
+  }
+}
+
+class ActivatedRouterStub {
+  params: Observable<any> = Observable.empty();
+}
+
+describe('UserDetailsComponent', () => {
+  let component: UserDetailsComponent;
+  let fixture: ComponentFixture<UserDetailsComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ UserDetailsComponent ],
+      providers: [
+        { provide: Router, useClass: RouterStub },
+        { provide: ActivatedRoute, useClass: ActivatedRouterStub },
+      ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(UserDetailsComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should redirect the user to the users page after saving', () => {
+    const router = TestBed.get(Router);
+    const spy = spyOn(router, 'navigate');
+
+    component.save();
+
+    expect(spy).toHaveBeenCalledWith(['users']);
+  });
+});
